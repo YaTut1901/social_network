3d3595a32222cbbc2b8e84137e12a77faaeaaf74
YaTut1901 <ovanya1901@gmail.com>
2021-10-10 20:09:52 +0300

    make-jenkins-to-build-with-maven

--- commit-and-push.sh
+++ commit-and-push.sh
@@ -2,10 +2,13 @@ git commit -am "$1"
 git push
 
 #------------------------------------------------------------------------------------------------------------------------
+if [[ $2 == "true" ]] ; then
+  return
+fi
 
 USER=ivan
 TOKEN=1152347fd2957db1f021c69bc26400f7f8
 
 CRUMB=$(curl --user ivan:1152347fd2957db1f021c69bc26400f7f8 http://localhost:8080/crumbIssuer/api/json | jq -r '.crumb')
 
-curl -X POST http://localhost:8080/job/test/build?token=test -H "Jenkins-Crumb:"$CRUMB --user $USER:$TOKEN
+curl -X POST http://localhost:8080/job/social_network/build?token=buildme -H "Jenkins-Crumb:"$CRUMB --user $USER:$TOKEN
