389265e291740eacdcabdca7ef1727e8889690dc
YaTut1901 <ovanya1901@gmail.com>
2021-10-10 20:24:15 +0300

    make-jenkins-to-build-with-maven

--- commit-and-push.sh
+++ commit-and-push.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
+COMMIT_MESSAGE=$1
+DO_JENKINS_BUILD_ENABLED=$2
 
-git commit -am "$1"
+git commit -am "$COMMIT_MESSAGE"
 git push
 
 #------------------------------------------------------------------------------------------------------------------------
-echo "$2"
-if [ "$2" = "true" ] ; then
-  echo "exiting";
+if [ "$DO_JENKINS_BUILD_ENABLED" = "false" ] ; then
   exit 1;
 fi
 
@@ -15,4 +15,4 @@ TOKEN=1152347fd2957db1f021c69bc26400f7f8
 
 CRUMB=$(curl --user ivan:1152347fd2957db1f021c69bc26400f7f8 http://localhost:8080/crumbIssuer/api/json | jq -r '.crumb')
 
-curl -X POST http://localhost:s8080/job/social_network/build?token=buildme -H "Jenkins-Crumb:"$CRUMB --user $USER:$TOKEN
+curl -X POST http://localhost:8080/job/social_network/build?token=buildme -H "Jenkins-Crumb:"$CRUMB --user $USER:$TOKEN
